98cbafb961592a848b895b93ffad5a082e87bbc9
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-07-25 11:15:40 -0600

    Add place details service

--- local_authorities.rb
+++ local_authorities.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'sinatra/base'
 require 'json'
 require './services/places_service'
-# require './servcies/place_details_service'
+require './services/place_details_service'
 require 'pry'
 
 class LocalAuthorities < Sinatra::Base
@@ -13,7 +13,10 @@ class LocalAuthorities < Sinatra::Base
   end
 
   get '/sheriff' do
-    PlacesService.new.sheriff_search(request.params)
+    authorities = PlacesService.new.sheriff_search(request.params)
+    authorities['phone'] = PlaceDetailsService.new.phone(authorities['place_id'])
+    response['nearest_authority'] = authorities.to_json
+   
   end
 
 end
